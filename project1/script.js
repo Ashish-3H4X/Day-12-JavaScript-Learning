141e54f235e6f77d7b41b1d27a72c427ce5015bc
Mr Rahul <ashish227087@gmail.com>
2025-04-17 14:47:16 +0530

    Some important String method

---
+++ project1/script.js
@@ -0,0 +1,37 @@
+// length method 
+
+var  str = "I love JavaScript";
+ demo1.innerHTML =`var  str = "I love JavaScript";<br>
+ demo1.innerHTML = str.length <br>Result :<br>${str.length}`
+
+// String[] method 
+
+var  str = "I love JavaScript";
+ demo2.innerHTML= `var  str = "I love JavaScript";<br>
+ demo1.innerHTML = str[4] <br>Result :<br>${str[4]}`
+
+ // string Slice method 
+
+ var  str = "I love JavaScript";
+ demo3.innerHTML=`var  str = "I love JavaScript";<br>
+ demo1.innerHTML = str.slice(4,10)<br> Result :<br>${str.slice(4,10)}`
+
+ var  str = "I love JavaScript";
+ demo4.innerHTML=`var  str = "I love JavaScript";<br>
+ demo1.innerHTML = str.slice(4)<br> Result :<br>${str.slice(4)}`
+
+
+ // if  we use negative value then  negative value is start from -1 and goes  towards till last 
+ var  str = "I love JavaScript";
+ demo5.innerHTML=`var  str = "I love JavaScript";<br>
+ demo1.innerHTML = str.slice(-1))<br> Result :<br>${str.slice(-1)}` // for this -1 return (t) -2 return (pt) -3 return (ipt)
+
+  
+ var  str = "I love JavaScript";
+ demo6.innerHTML=`var  str = "I love JavaScript";<br>
+ demo1.innerHTML = str.slice(-16,-7)<br> Result :<br>${str.slice(-16,-7)}`
+
+
+ // substring () method 
+
+ 
